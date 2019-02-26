98da0ceeb3a1fd198fa5eadde5ac3aef995213a5
Armurier <45574381+Armurier@users.noreply.github.com>
2019-02-26 21:22:21 +0200

    Readding the program

---
+++ Noms de code frankfurt palermo.py
@@ -0,0 +1,32 @@
+from time import sleep
+from playsound import playsound
+frapal = {
+    "thierry": "Néphron",
+    "georges": "Sasuke",
+    "may": "Decembre",
+    "hala": "Etagere",
+    "marilou": "Anaconda",
+    "miali": "Freedom",
+    "carlo": "Gringe",
+    "bridi": "Salvitex",
+    "cathia": "Amsterdam",
+    "albert": "hurlu",
+    "basma": "Riz",
+    "nasr": "Determination",
+    "klink": "Benalla",
+    "traboulsi": "mate20pro",
+    "tripitch": "Freeze",
+    "chalfoun": "Kechek",
+    "sarah": "colle"
+}
+def decodeur(code):
+        if code in frapal:
+            sleep(1)
+            code2 = code.title()
+            print(f"{code2} a pour nom de code {frapal[code]}")
+            sleep(1)
+while True:
+    print("Quelle personne voulez-vous savoir parmi les personnes du voyage")
+    code = input()
+    code = code.lower()
+    decodeur(code)
